e37280fa82f7e76b3b48b37f873818959afdd093
ivorsimstilt <39617577+ivorsimstilt@users.noreply.github.com>
2018-10-31 14:34:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function main() {
 
   let originalImage = { width: 1, height: 1 }; // replaced after loading
   const originalTexture = twgl.createTexture(gl, {
-    src: "https://robindelaporte.fr/codepen/dog1.jpg", 
+    src: "../img/conflict.jpg", 
     crossOrigin: '',
   }, (err, texture, source) => {
     originalImage = source;
   });
   
   const mapTexture = twgl.createTexture(gl, {
-    src: "https://robindelaporte.fr/codepen/dog3.jpg", crossOrigin: '',
+    src: "../img/conflict_depth.jpg", crossOrigin: '',
   });
   
 
